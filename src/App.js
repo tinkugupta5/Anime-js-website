46a5b67754cfc12d21491a2be45c43b7f5dbbcc8
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-20 01:04:37 +0530

    adding commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import anime from "animejs";
+import "./style.css";
 
-function App() {
+const AnimeComponent = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+   
+    container.innerHTML = "";
+
+    for (let i = 1; i <= 100; i++) {
+      let dot = document.createElement("div");
+      dot.classList.add("element");
+      container.appendChild(dot);
+    }
+
+    let dotAll = container.querySelectorAll(".element");
+    let animation = anime.timeline({
+      targets: dotAll,
+      easing: "easeInOutExpo",
+      loop: true,
+      delay: anime.stagger(100, { grid: [10, 10], from: "center" }),
+    });
+
+    animation
+      .add({
+        rotateZ: 180,
+        translateY: anime.stagger(0, { grid: [10, 10], from: "center", axis: "y" }),
+        translateX: anime.stagger(0, { grid: [10, 10], from: "center", axis: "x" }),
+        opacity: 1,
+      })
+      .add({
+        borderRadius: 50,
+      })
+      .add({
+        scale: 0.2,
+        opacity: 0.2,
+      })
+      .add({
+        rotateZ: 180,
+        translateY: anime.stagger(0, { grid: [10, 10], from: "center", axis: "y" }),
+        translateX: anime.stagger(0, { grid: [10, 10], from: "center", axis: "x" }),
+        opacity: 1,
+      })
+      .add({
+        scale: 1,
+        borderRadius: 0,
+      })
+      .add({
+        rotateZ: -90,
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <header>
+        <a href="#" className="logo">
+          Logo
         </a>
+        <ul>
+          <li>
+            <a href="#" className="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Work</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
       </header>
+
+      <section>
+        <div className="content">
+          <h2>
+            Level Up Your Website <b>Anime.js</b>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam possimus dolorem maiores vitae fuga.
+            Fuga cumque voluptates vero commodi quia excepturi eligendi asperiores fugit hic ipsa, dolores explicabo
+            consequuntur neque.
+          </p>
+          <a href="#" className="btn">
+            Learn More
+          </a>
+        </div>
+        <div className="container" ref={containerRef}></div>
+      </section>
     </div>
   );
-}
+};
 
-export default App;
+export default AnimeComponent;
